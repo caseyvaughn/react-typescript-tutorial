b70029623eacf685817ae97f7ed8a91ba0f8ece5
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-04-07 14:18:44 -0400

    saving before adding reducer hook

--- typescript-react-example/src/TextField.tsx
+++ typescript-react-example/src/TextField.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 //specify what props it takes
 //can also pass an object directly into React, but more common to declare an interface
@@ -16,15 +16,30 @@ interface Props {
   fn?: (bob: string) => string;
   //use an arrow function to set a function; can return void or specify the data type
   person: Person; //utilizes the Person interface that sets up an object
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const TextField: React.FC<Props> = ({ }) => {
+interface TextNode {
+  text: string;
+}
+
+export const TextField: React.FC<Props> = ({ handleChange}) => {
   //adding : React.FC makes TextField a React functional component
-  
-  const [count, setCount] = useState(5) //typescript reads the 5 and infers that the type for count & setCount is a number
+
+  // const [count, setCount] = useState<number | null>(5)
+  //typescript reads the 5 and infers that the type for count & setCount is a number
+  //use <number | null> to declare the data type as a number OR null
+  //note: undefined is different than null
+
+  const [count, setCount] = useState<TextNode>({ text: 'hello' })
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      <input/>
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange}/>
+      {/* pro-tip: hovering over the ref will show what element it is expecting (<HTMLInputElement>) that gets placed above next to useRef */}
+
     </div>
   )
 }
